1b01dd94831759933aafb02deecef13c496921f8
risingstar <risingstar1002@mail.ru>
2020-04-23 12:53:22 +1000

    Add Redux-Saga

--- src/index.js
+++ src/index.js
@@ -7,14 +7,16 @@ import { BrowserRouter } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import rootReducer from "./modules";
+import createSagaMiddleware from "redux-saga";
+import rootReducer, { rootSaga } from "./modules";
 
+const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   rootReducer,
   window.__PRELOADED_STATE__,
-  applyMiddleware(thunk)
+  applyMiddleware(thunk, sagaMiddleware)
 );
-
+sagaMiddleware.run(rootSaga);
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
